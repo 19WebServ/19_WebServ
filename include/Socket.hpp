813507b66be9c9791fb28d1427707f16978eadf5
Nico <nicolas.dessard@hotmail.com>
2024-12-16 15:21:17 +0100

    fix: back to previous version

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -7,56 +7,25 @@
 #include <unistd.h>
 #include <sstream>
 
-// class Socket 
-// {
-//     public:
-//         Socket(int port, unsigned int ip);
-//         ~Socket();
-
-//         int     createSocket();
-//         int     bindSocket();
-//         int     listenSocket(int backlog = 5); //backlog -> nombre deconnexion en attente pouvant etre géré
-//         int     acceptConnection();
-//         void    closeSocket();
-
-//         int     sendData(int target_sock, const char *data, unsigned int len);
-//         int     receiveData(int target_sock, char *buffer, unsigned int len);
-
-//         std::string getClientIP(struct sockaddr_in *client_addr);
-
-//     private:
-//         int _server_sock;
-//         struct sockaddr_in _server_addr;
-// };
-
 class Socket 
 {
     public:
-        Socket();
-        virtual ~Socket();
-        int     sendData(int target_sock, const char *data, unsigned int len);
-        int     receiveData(int target_sock, char *buffer, unsigned int len);
-        void    closeSocket();
-    
-    protected:
-        int _socket_fd;
-        sockaddr_in _addr;
-};
+        Socket(int port, unsigned int ip);
+        ~Socket();
 
-class ServerSocket: public Socket
-{
-    public:
-        ServerSocket(int port);
+        int     createSocket();
         int     bindSocket();
         int     listenSocket(int backlog = 5); //backlog -> nombre deconnexion en attente pouvant etre géré
         int     acceptConnection();
+        void    closeSocket();
+
+        int     sendData(int target_sock, const char *data, unsigned int len);
+        int     receiveData(int target_sock, char *buffer, unsigned int len);
+
         std::string getClientIP(struct sockaddr_in *client_addr);
 
+    private:
+        int _server_sock;
+        struct sockaddr_in _server_addr;
 };
 
-class ClientSocket : public Socket
-{
-    public:
-        ClientSocket(const std::string &host, int port);
-        void connectToServer();
-};
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,26 @@
 #include "../include/Socket.hpp"
-#include <iostream>
-#include <cstring>
 
-int init_server(ServerSocket &server) 
-{
-    if (server.bindSocket() < 0 || server.listenSocket(10) < 0)
+int init_server(Socket &server) {
+    if (server.createSocket() < 0 || server.bindSocket() < 0 || server.listenSocket() < 0) 
         return 1;
     std::cout << "Server is listening on port 8080...\n" << std::endl;
     return 0;
 }
 
-void handle_client(int client_sock, ServerSocket &server) 
-{
+void handle_client(int client_sock, Socket &server) {
     char buffer[1024] = {0};
-
-    int bytes_received = server.receiveData(client_sock, buffer, sizeof(buffer) - 1);
-    if (bytes_received > 0) 
+    int bytes_receiv = server.receiveData(client_sock, buffer, sizeof(buffer));
+    if (bytes_receiv > 0) 
     {
-        buffer[bytes_received] = '\0';
+        buffer[bytes_receiv] = '\0';
         std::cout << "Received from client: " << buffer << std::endl; 
-    }
-    else if (bytes_received == 0)
+    } 
+    else if (bytes_receiv == 0)
         std::cout << "Client disconnected." << std::endl; 
     else
         std::cerr << "Failed to receive data from client." << std::endl;
 
-    const char *response =
+    const char* response =
         "HTTP/1.1 200 OK\r\n"
         "Content-Type: text/plain\r\n"
         "Content-Length: 15\r\n"
@@ -41,23 +36,23 @@ void handle_client(int client_sock, ServerSocket &server)
     std::cout << "Client socket closed." << std::endl;
 }
 
-int main() 
-{
-    ServerSocket server(8080);
-
+int main() {
+    Socket server(8080, 0);
     if (init_server(server))
         return -1;
+
+    // Boucle pour accepter et gérer plusieurs connexions
     while (true) {
         int client_sock = server.acceptConnection();
-        if (client_sock < 0) 
-        {
+        if (client_sock < 0) {
             std::cerr << "Failed to accept client connection." << std::endl;
             continue;
         }
-
         std::cout << "New client connected!" << std::endl;
+
         handle_client(client_sock, server);
     }
+
     server.closeSocket();
     return 0;
 }
